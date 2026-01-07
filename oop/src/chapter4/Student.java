1452edc52451563487cdbc6da7fba37a0d8f4bf2
phamminhhieupc <phamminhhieupc@gmail.com>
2026-01-07 23:28:23 +0700

    oop_7/1

--- oop/src/chapter4/Student.java
+++ oop/src/chapter4/Student.java
@@ -1,31 +1,78 @@
 package chapter4;
 
-public class Student {
-    String name;
-    int age;
+abstract class SinhVien {
+    protected String id;
+    protected String name;
+    protected double price;
+    protected double tax;
 
-    public Student(String name, int age) {
+    abstract void tinhDiem();
+
+    public SinhVien(String id, String name, double price, double tax) {
+        this.id = id;
         this.name = name;
-        this.age = age;
+        this.price = price;
+        this.tax = tax;
     }
 
-    public String getName() {
-        return name;
+    public double getPriceTax() {
+        return this.price * this.tax;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void show() {
+    public void info() {
         System.out.println("hello");
     }
+}
+
+class SinhVienIT extends SinhVien {
+    private String language;
+
+    public SinhVienIT(String id, String name, double price, double tax, String language) {
+        super(id, name, price, tax);
+        this.language = language;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    void tinhDiem() {
+
+    }
+}
+
+class SinhVienCoKhi extends SinhVien {
+    private String skill;
+
+    public SinhVienCoKhi(String id, String name, double price, double tax, String skill) {
+        super(id, name, price, tax);
+        this.skill = skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    @Override
+    void tinhDiem() {
+        
+    }
+}
+
+public class Student {
+    public static void main(String[] args) {
+        SinhVienIT sv = new SinhVienIT("1", "hieu", 10, 0.2, "java");
+        System.out.println(sv.getPriceTax());
+    }
+
+
 }
